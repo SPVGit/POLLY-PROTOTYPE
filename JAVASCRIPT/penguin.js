fd4aff3c8c185c6f80de522494410eb2666a84da
Subarna Paul Vignarajah <subarnapaul@rocketmail.com>
2023-01-15 21:34:16 +0000

    background audio

--- JAVASCRIPT/penguin.js
+++ JAVASCRIPT/penguin.js
@@ -1,9 +1,19 @@
 
 //INITIALIZE CODE------------------------------------------------------------------------------
 
+let backgroundAudio = new Audio('PRICE-OF-FREEDOM.mp3')
+
+function playAudio() {
+    backgroundAudio.play();
+  }
+
+function stopAudio(){
+    backgroundAudio.pause();
+    backgroundAudio.currentTime = 0;
+    backgroundAudio.src = ""
+}
 
 let PINTRO, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P10END
-
 
 PINTRO = document.getElementById('P-INTRO')
 P1 = document.getElementById('P-1')
@@ -19,7 +29,59 @@ P10 = document.getElementById('P-10')
 P10END = document.getElementById('P-10-THE-END')
 
 
+let pagesArray = [PINTRO,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10]
+//let buttonsArray = [P1btn, P2btn, P3btn, P4btn, P5btn, P6btn, P7btn, P8btn, P9btn, P10btn]
 
-function openNewCard(){
+let buttonsArray = [...document.getElementsByClassName('btn-style')]
+
+
+
+let points = [...document.getElementsByTagName('span')]
+
+
+
+function openNewCard(btnArray, pgsArray){
+
+    for (let item of btnArray){
+        
+        
+        for(let element of pgsArray){
+           
+            if (item.classList.contains(element.id)){
+            
+                item.onclick=function(){
+                    item.parentNode.parentNode.style.display="none"
+                    element.style.display='flex'
+                    if(item.id==="P-1btn"){
+                        playAudio()
+                    }
+                    else if(item.classList.contains("P-INTRO")){
+                        points.forEach(span=>span.innerHTML=0)
+                        stopAudio()
+                        location.reload();
+                    }
+                    else{
+                        if (parseFloat(item.parentNode.parentNode.id.substring(2,9))<parseFloat(element.id.substring(2,9))){
+                            points.forEach(span=>span.innerHTML=parseFloat(span.innerHTML)+1)
+                        }
+                        else if(parseFloat(item.parentNode.parentNode.id.substring(2,9))>parseFloat(element.id.substring(2,9))){
+                            points.forEach(span=>span.innerHTML=parseFloat(span.innerHTML)-1)
+                        }
+                    }
+                    
+                   
+                }
+            
+            }
+        }
+        
+    }
     
 }
+
+openNewCard(buttonsArray,pagesArray)
+
+
+
+
+ 
