bc1ee7ca301c87b59da20296b8600b330fc60835
Subarna Paul Vignarajah <subarnapaul@rocketmail.com>
2023-01-15 16:19:25 +0000

    done

---
+++ JAVASCRIPT/penguin.js
@@ -0,0 +1,25 @@
+
+//INITIALIZE CODE------------------------------------------------------------------------------
+
+
+let PINTRO, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P10END
+
+
+PINTRO = document.getElementById('P-INTRO')
+P1 = document.getElementById('P-1')
+P2 = document.getElementById('P-2')
+P3 = document.getElementById('P-3')
+P4 = document.getElementById('P-4')
+P5 = document.getElementById('P-5')
+P6 = document.getElementById('P-6')
+P7 = document.getElementById('P-7')
+P8 = document.getElementById('P-8')
+P9 = document.getElementById('P-9')
+P10 = document.getElementById('P-10')
+P10END = document.getElementById('P-10-THE-END')
+
+
+
+function openNewCard(){
+    
+}
